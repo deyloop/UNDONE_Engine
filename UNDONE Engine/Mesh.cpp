347f7e684157bcc410cd9ff7414668cc2ab1ba2c
Anurup Dey <anu.rup.dey98@gmail.com>
2015-03-07 11:26:22 +0530

    Made Assimp Load my models.

--- UNDONE Engine/Mesh.cpp
+++ UNDONE Engine/Mesh.cpp
@@ -44,96 +44,46 @@ namespace UNDONE_ENGINE {
 
 		Assimp::Importer importer;
 		const aiScene* scene = importer.ReadFile(m_model_file.c_str(),
-										  aiProcess_CalcTangentSpace|
-										  aiProcess_Triangulate|
-										  aiProcess_JoinIdenticalVertices|
-										  aiProcess_SortByPType);
+										aiProcess_Triangulate|
+										aiProcess_SortByPType);
 		if (!scene) return;
-		std::vector< unsigned int > vertexIndices, uvIndices, normalIndices;
-		std::vector< glm::vec3 > vertices;
-		std::vector< glm::vec2 > uvs;
-		std::vector< glm::vec3 > normals;
+		std::vector< float > vertices;
+		std::vector< float > uvs;
+		std::vector< float > normals;
 
-		FILE* file; 
-		fopen_s(&file, m_model_file.c_str( ), "r");
-		if (file==nullptr) {
-			coutput("Unable to open mesh file: "+m_model_file.c_str( )+"\n");
-			return;
-		}
-		while (1) {
+		m_num_verts = 0;
 
-			char lineHeader[128];
-			// read the first word of the line
-			int res = fscanf_s(file, "%s", lineHeader,128);
-			if (res==EOF)
-				break; // EOF = End Of File. Quit the loop.
-
-			// else : parse lineHeader
-
-			if (strcmp(lineHeader, "v")==0) {
-				glm::vec3 vertex;
-				fscanf_s(file, "%f %f %f\n", &vertex.x, &vertex.y, &vertex.z);
-				vertices.push_back(vertex);
-			} else if (strcmp(lineHeader, "vt")==0) {
-				glm::vec2 uv;
-				fscanf_s(file, "%f %f\n", &uv.x, &uv.y);
-				uvs.push_back(uv);
-			} else if (strcmp(lineHeader, "vn")==0) {
-				glm::vec3 normal;
-				fscanf_s(file, "%f %f %f\n", &normal.x, &normal.y, &normal.z);
-				normals.push_back(normal);
-			} else if (strcmp(lineHeader, "f")==0) {
-				std::string vertex1, vertex2, vertex3;
-				unsigned int vertexIndex[3], uvIndex[3], normalIndex[3];
-				int matches = fscanf_s(file, "%d//%d %d//%d %d//%d\n", &vertexIndex[0] ,&normalIndex[0], &vertexIndex[1], &normalIndex[1], &vertexIndex[2], &normalIndex[2]);
-				if (matches!=6) {
-					printf("File can't be read by our simple parser : ( Try exporting with other options)\n");
-					return;
-				}
-				vertexIndices.push_back(vertexIndex[0]);
-				vertexIndices.push_back(vertexIndex[1]);
-				vertexIndices.push_back(vertexIndex[2]);
-				/*
-				uvIndices.push_back(uvIndex[0]);
-				uvIndices.push_back(uvIndex[1]);
-				uvIndices.push_back(uvIndex[2]);
-				*/
-				normalIndices.push_back(normalIndex[0]);
-				normalIndices.push_back(normalIndex[1]);
-				normalIndices.push_back(normalIndex[2]);
+		aiMesh* mesh = scene->mMeshes[0];
+		int iMeshFaces = mesh->mNumFaces;
+		for (int j = 0; j<iMeshFaces; ++j) {
+			const aiFace& face = mesh->mFaces[j];
+			for (int k = 0; k<3; ++k) {
+				aiVector3D pos = mesh->mVertices[face.mIndices[k]];
+				//aiVector3D uv = mesh->mTextureCoords[0][face.mIndices[k]];
+				aiVector3D normal = mesh->HasNormals( ) ? mesh->mNormals[face.mIndices[k]] : aiVector3D(1.0f, 1.0f, 1.0f);
+				vertices.push_back(pos.x);
+				vertices.push_back(pos.y);
+				vertices.push_back(pos.z);
+				//uvs.push_back(uv);
+				normals.push_back(normal.x);
+				normals.push_back(normal.y);
+				normals.push_back(normal.z);
+				++m_num_verts;
 			}
 		}
-
-		vector<glm::vec3> verts;
-		vector<glm::vec3> vnormals;
-		// For each vertex of each triangle
-		for (unsigned int i = 0; i<vertexIndices.size( ); i++) {
-			unsigned int vertexIndex = vertexIndices[i];
-			glm::vec3 vertex = vertices[vertexIndex-1];
-			verts.push_back(vertex);
-		}
-
-		for (unsigned int i = 0; i<normalIndices.size( ); i++) {
-			unsigned int normalIndex = normalIndices[i];
-			glm::vec3 normal = normals[normalIndex-1];
-			vnormals.push_back(normal);
-		}
-
-
-		m_num_verts = verts.size( );
-
+		
 		glGenVertexArrays(1, uiVAO);
 		glGenBuffers(2, uiVBO);
 		// Setup whole cube
 		glBindVertexArray(uiVAO[0]);
 
 		glBindBuffer(GL_ARRAY_BUFFER, uiVBO[0]);
-		glBufferData(GL_ARRAY_BUFFER, verts.size()*sizeof(float)*3, &verts[0], GL_STATIC_DRAW);
+		glBufferData(GL_ARRAY_BUFFER, vertices.size()*sizeof(float), &vertices[0], GL_STATIC_DRAW);
 		glEnableVertexAttribArray(0);
 		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
 		glBindBuffer(GL_ARRAY_BUFFER, uiVBO[1]);
-		glBufferData(GL_ARRAY_BUFFER, vnormals.size()*sizeof(float)*3, &vnormals[0], GL_STATIC_DRAW);
+		glBufferData(GL_ARRAY_BUFFER, normals.size()*sizeof(float), &normals[0], GL_STATIC_DRAW);
 		glEnableVertexAttribArray(1);
 		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
