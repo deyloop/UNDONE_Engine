df0c185ba4ea8667f2a2143662e1eff6957ab36d
Anurup Dey <anu.rup.dey98@gmail.com>
2015-04-28 13:50:43 +0530

    Added Scene.h and made the Scene Interface.

---
+++ UNDONE Engine/Scene.h
@@ -0,0 +1,26 @@
+/******************************************************************************
+Project	:	UNDONE Engine
+File	:	Scene.h
+Author	:	Anurup Dey
+******************************************************************************/
+///////////////////////////////////////////////////////////////////////////////
+#pragma once 
+
+#ifndef UNDONE_SCENE_H
+#define UNDONE_SCENE_H
+
+namespace UNDONE_ENGINE{
+/*-----------------------------------------------------------------------------
+The scene represents a segment of the game or application having its own memory
+space and set of objects.Think of them as separate levels. They can be loaded 
+samultaneousely and freed independent of each other. THis is an abstract 
+interface. The User can create any behavior they want in a scene.
+-----------------------------------------------------------------------------*/
+	class Scene {
+	public:
+		virtual void Load( )	= 0;
+		virtual void UnLoad( )	= 0;
+		virtual void Release( ) = 0;
+	};
+}
+#endif
