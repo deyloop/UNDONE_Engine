42ed08baa4a47ba0ccf78782a62907b9bccb8a20
Dey <Dey@DESKTOP-Q3H6PL8>
2015-08-11 12:04:16 +0530

    Revert "Updated Copyright Notices"
    This reverts commit 5107d82d1b68ec49f8b920c7b9349ab0238ae49e.

--- UNDONE Engine/GraphicsEngine.cpp
+++ UNDONE Engine/GraphicsEngine.cpp
@@ -2,16 +2,21 @@
 File	:	GraphicsEngine.cpp
 Author	:	Anurup Dey
 
-		Copyright (C) 2015 Anurup Dey 
-		<anu.rup.dey98@gmail.com>
+				Copyright (C) 2015  Anurup Dey
 
-	This file is part of UNDONE-Engine.
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
 
-	UNDONE-Engine can not be copied and/or distributed without the express
-	permission of Anurup Dey. Unauthorized copying of this file, via any
-	medium is strictly prohibited.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-	Proprietary and confidential.
+	You should have received a copy of the GNU General Public License along
+	with this program; if not, write to the Free Software Foundation, Inc.,
+	51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 ******************************************************************************/
 ///////////////////////////////////////////////////////////////////////////////
